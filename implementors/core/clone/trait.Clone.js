04815523c1188572e18b1ff233349396ab8353b8
Travis CI User <travis@example.org>
2020-10-06 22:28:03 +0000

    Update documentation

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["packed_simd_2"] = [{"text":"impl&lt;A:&nbsp;Clone + SimdArray&gt; Clone for Simd&lt;A&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;Clone&gt; Clone for LexicographicallyOrdered&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl Clone for m8","synthetic":false,"types":[]},{"text":"impl Clone for m16","synthetic":false,"types":[]},{"text":"impl Clone for m32","synthetic":false,"types":[]},{"text":"impl Clone for m64","synthetic":false,"types":[]},{"text":"impl Clone for m128","synthetic":false,"types":[]},{"text":"impl Clone for msize","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["packed_simd_2"] = [{"text":"impl&lt;A:&nbsp;Copy + SimdArray&gt; Copy for Simd&lt;A&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;Copy&gt; Copy for LexicographicallyOrdered&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl Copy for m8","synthetic":false,"types":[]},{"text":"impl Copy for m16","synthetic":false,"types":[]},{"text":"impl Copy for m32","synthetic":false,"types":[]},{"text":"impl Copy for m64","synthetic":false,"types":[]},{"text":"impl Copy for m128","synthetic":false,"types":[]},{"text":"impl Copy for msize","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
